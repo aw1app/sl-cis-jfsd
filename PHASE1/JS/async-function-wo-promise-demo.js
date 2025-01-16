8bded96bef93ce843c7ad8f3a379b5ae294ee82f
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2025-01-16 12:21:31 +0530

    updates

--- PHASE1/JS/async-function-wo-promise-demo.js
+++ PHASE1/JS/async-function-wo-promise-demo.js
@@ -4,7 +4,7 @@
  async function fetchUserData() {
     try {
         console.log("Fetching user data...");
-        const response = await fetch("https://jsonplaceholder.typicode.com/todos/1"); // API call
+        const response = await fetch("https://jsonplaceholder.typicode.com/users/1"); // API call
         if (!response.ok) {
             throw new Error(`HTTP error! Status: ${response.status}`);
         }
