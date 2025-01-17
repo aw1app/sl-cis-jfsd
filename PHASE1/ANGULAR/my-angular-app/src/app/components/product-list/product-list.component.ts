e552899bec283e9dd5d510ec84409c5990ca8a91
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2025-01-17 10:05:13 +0530

    updates

--- PHASE1/ANGULAR/my-angular-app/src/app/components/product-list/product-list.component.ts
+++ PHASE1/ANGULAR/my-angular-app/src/app/components/product-list/product-list.component.ts
@@ -7,6 +7,7 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule],
   templateUrl: './product-list.component.html',
+  // template:'<p>Hi</p>',
   styleUrl: './product-list.component.css'
 })
 export class ProductListComponent {
@@ -16,10 +17,14 @@ export class ProductListComponent {
   constructor(){
     console.log("Hi");
     this.products= [];
-    this.products.push(new Product(1, "HP Laptop",20000, "Gaming Laptop",true,"IMAGES\hp.jpg"));
+    this.products.push(new Product(1, "HP Laptop",20000, "Gaming Laptop",true,"IMAGES/hp-laptop.jpg"));
     this.products.push(new Product(2, "Samsumg Laptop",40000, "Gaming Laptop",false,"IMAGES\hp.jpg"));
     this.products.push(new Product(3, "Lenova Laptop",20500, "Gaming Laptop",true,"IMAGES\hp.jpg"));
     this.products.push(new Product(3, "Sony Laptop",22000, "Gaming Laptop",true,"IMAGES\hp.jpg"));
   }
 
+  popOffProduct():void{
+    this.products.pop();
+  }
+  
 }
