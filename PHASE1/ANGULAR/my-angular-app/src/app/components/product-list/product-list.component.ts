f432f5f42226dfd81ae62991b1eae442901049bb
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2025-01-17 14:20:20 +0530

    updates

--- PHASE1/ANGULAR/my-angular-app/src/app/components/product-list/product-list.component.ts
+++ PHASE1/ANGULAR/my-angular-app/src/app/components/product-list/product-list.component.ts
@@ -21,10 +21,10 @@ export class ProductListComponent {
   constructor(){
     console.log("Hi");
     this.products= [];
-    this.products.push(new Product(1, "HP Laptop",20000, "Gaming Laptop",true,"IMAGES/hp-laptop.jpg"));
-    this.products.push(new Product(2, "Samsumg Laptop",40000, "Gaming Laptop",false,"IMAGES\hp.jpg"));
-    this.products.push(new Product(3, "Lenova Laptop",20500, "Gaming Laptop",true,"IMAGES\hp.jpg"));
-    this.products.push(new Product(3, "Sony Laptop",22000, "Gaming Laptop",true,"IMAGES\hp.jpg"));
+    // this.products.push(new Product(1, "HP Laptop",20000, "Gaming Laptop",true,"IMAGES/hp-laptop.jpg"));
+    // this.products.push(new Product(2, "Samsumg Laptop",40000, "Gaming Laptop",false,"IMAGES\hp.jpg"));
+    // this.products.push(new Product(3, "Lenova Laptop",20500, "Gaming Laptop",true,"IMAGES\hp.jpg"));
+    // this.products.push(new Product(3, "Sony Laptop",22000, "Gaming Laptop",true,"IMAGES\hp.jpg"));
   }
 
   submitBid(product: Product) {
