5196d9f4af54e5b2abd7013f80bda83d59304e5b
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2025-01-22 12:15:41 +0530

    updates

--- PHASE1/ANGULAR/angular-app2/src/app/app.routes.ts
+++ PHASE1/ANGULAR/angular-app2/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { AddProductComponent } from './components/add-product/add-product.component';
+import { NoSuchComponent } from './components/no-such/no-such.component';
 
 export const routes: Routes = [
 
@@ -9,5 +10,5 @@ export const routes: Routes = [
     { path: 'list', component: ProductListComponent },
     { path: 'add', component: AddProductComponent },
 
-    // { path: '**', component: AddProductComponent },
+    { path: '**', component: NoSuchComponent },
 ];
---
+++ PHASE1/ANGULAR/angular-app2/src/app/components/no-such/no-such.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-no-such',
+  standalone: true,
+  imports: [],
+  templateUrl: './no-such.component.html',
+  styleUrl: './no-such.component.css'
+})
+export class NoSuchComponent {
+
+}
