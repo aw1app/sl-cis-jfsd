f3820612ffd7031ce33f9cfb69d6c29fa2f03151
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2025-01-22 14:38:56 +0530

    updates

--- PHASE1/ANGULAR/angular-app2/src/app/app.routes.ts
+++ PHASE1/ANGULAR/angular-app2/src/app/app.routes.ts
@@ -4,13 +4,16 @@ import { ProductListComponent } from './components/product-list/product-list.com
 import { AddProductComponent } from './components/add-product/add-product.component';
 import { NoSuchComponent } from './components/no-such/no-such.component';
 import { ProductComponent } from './components/product/product.component';
+import { PipesdemoComponent } from './components/pipesdemo/pipesdemo.component';
 
 export const routes: Routes = [
 
-    { path: '', component: AppComponent },
-    { path: 'details/:id', component: ProductComponent},
+    { path: '', component: AppComponent },    
     { path: 'list', component: ProductListComponent },
     { path: 'add', component: AddProductComponent },
+    { path: 'details/:id', component: ProductComponent},
+    { path: 'pipes-demo', component: PipesdemoComponent},
+    
 
     { path: '**', component: NoSuchComponent },
 ];
---
+++ PHASE1/ANGULAR/angular-app2/src/app/components/pipesdemo/pipesdemo.component.ts
@@ -0,0 +1,42 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { ProductService } from '../../services/product.service';
+import { Product } from '../../models/product';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-pipesdemo',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './pipesdemo.component.html',
+  styleUrl: './pipesdemo.component.css'
+})
+export class PipesdemoComponent {
+
+  title = 'Angular Pipes Demo';
+  today = new Date();
+  price = 12345.6789;
+  percentage = 0.8567;
+  longText = 'This is a very long text that needs slicing for display purposes.';
+  jsonData = { name: 'John', age: 30, job: 'Engineer' };
+  items = ['Item 1', 'Item 2', 'Item 3', 'Item 4'];
+
+  // For custom pipe demo
+  customMessage = 'hello world!';
+
+  asyncProducts!: Observable<Product[]>;
+
+  productService!:ProductService;
+
+  constructor(productService:ProductService){
+    this.productService = productService;
+  }
+
+  ngOnInit():void {
+    // this.products = this.productService.getAllProducts();
+
+    this.asyncProducts=this.productService.getAllProducts();
+
+  }
+
+}
