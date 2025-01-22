53df600e4494d76e7e4d12be919fef33bf923589
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2025-01-22 16:01:56 +0530

    updates

---
+++ PHASE1/REACT/my-app2/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Welcome to Amazon EStore
+      </header>
+
+      <Welcome />
+    </div>
+  );
+}
+
+export default App;
---
+++ PHASE1/REACT/my-app2/src/components/Welcome.js
@@ -0,0 +1,14 @@
+
+import { Component } from 'react';
+
+class Welcome extends Component{
+
+return (
+
+    <p>Hi</p>
+)
+
+}
+
+
+export default Welcome;
