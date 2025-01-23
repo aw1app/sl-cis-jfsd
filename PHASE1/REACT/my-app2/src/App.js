987becb95b10e082a9dbfbb2fc66b248d0308efb
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2025-01-23 09:46:01 +0530

    react fix start up errors updates

--- PHASE1/REACT/my-app2/src/App.js
+++ PHASE1/REACT/my-app2/src/App.js
@@ -1,17 +1,26 @@
 // import logo from './logo.svg';
 import './App.css';
 import Welcome from './components/Welcome';
+import Product from './components/Product';
 
 function App() {
+
+
   return (
     <div className="App">
-      <header className="App-header">
+      <h1 >
         Welcome to Amazon EStore
-      </header>
+      </h1>
+
+      <Product name="Samsung Galaxy"  price="20000" />
+
+      <Product name="HP Laptop"  price="25000" />
 
       <Welcome />
+      
     </div>
   );
+
 }
 
 export default App;
---
+++ PHASE1/REACT/my-app2/src/components/Product.js
@@ -0,0 +1,29 @@
+import { Component } from "react";
+
+class Product extends Component {
+
+    productName = "Samsung Mobile";
+    productPrice = 0;
+
+
+    constructor(props){
+        super(props);
+
+        this.productName = this.props.name;
+        this.productPrice = this.props.price;
+    }
+
+
+    render(){
+
+        return (
+            <div>
+            <h2> Name : { this.productName }</h2>
+            <h3> Price : { this.productPrice }</h3>
+            </div>
+        )
+
+    }
+}
+
+export default Product;
