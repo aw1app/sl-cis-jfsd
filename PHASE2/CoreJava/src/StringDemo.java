7190650ebce2db94f7302656e83f054c0f71e4b4
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2025-02-19 15:21:53 +0530

    updates

--- PHASE2/CoreJava/src/StringDemo.java
+++ PHASE2/CoreJava/src/StringDemo.java
@@ -21,6 +21,35 @@ public class StringDemo {
 		System.out.println("Compare String s1.equals(s3):		 " + s1.equals(s3)); // false
 		System.out.println("Compare String s1.equalsIgnoreCase(s2):		 " + s1.equalsIgnoreCase(s2)); // true
 		System.out.println("Compare String s1.equalsIgnoreCase(s3):		 " + s1.equalsIgnoreCase(s3)); // false
+		
+		
+		
+		// Other methods
+		System.out.println("\n\nOther methds Demo");
+		String s11="Java is programming language";
+		System.out.println("s1.startsWith(\"Java\")" + s11.startsWith("Java")); // true
+		System.out.println(s11.endsWith("language")); // true
+		System.out.println(s11.substring(10)); // 10 is	starting index to end 
+		System.out.println(s11.indexOf("programming")); // 
+		System.out.println(s11.trim());
+		
+		
+		// Split a string into an array.
+		System.out.println("\n\n Split a string into an array.");
+		String s111="abc@test.com";
+		String[] s111Array=s111.split("@"); // divide string		based on @
+		for(String c:s111Array) // foreach loop
+		{
+		System.out.println(c);
+		}
+		
+		// Replacement
+		System.out.println("\n\n Replacement demo");
+		String str1="Heello";
+		String str1Replaced=str1.replace('e', 'k');
+		System.out.println(str1); //Hello 
+		System.out.println(str1Replaced); // Hkllo
+
 
 	}
 
