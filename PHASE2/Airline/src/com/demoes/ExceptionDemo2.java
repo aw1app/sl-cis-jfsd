abba77097733dd253ca0f3197dbc22bcd68ff524
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2025-02-21 15:48:04 +0530

    updates

--- PHASE2/Airline/src/com/demoes/ExceptionDemo2.java
+++ PHASE2/Airline/src/com/demoes/ExceptionDemo2.java
@@ -9,9 +9,10 @@ public class ExceptionDemo2 {
 	public static void main(String[] args) {
 
 		// int result = 10/0;
+		FileReader fr = null;
 
 		try {
-			FileReader fr = new FileReader("C:\\Users\\home\\sl-cis-jfsd\\PHASE2\\Airline\\src\\phone_nos.txt");
+			 fr = new FileReader("C:\\Users\\home\\sl-cis-jfsd\\PHASE2\\Airline\\src\\phone_nos.txt");
 
 			int c = fr.read();
 			
@@ -23,8 +24,21 @@ public class ExceptionDemo2 {
 		} catch (IOException e) {
 
 			System.out.println(" File not readable.");
+		} finally {
+			try {
+				fr.close();
+			} catch (IOException e) {				
+				e.printStackTrace();
+			}
 		}
 
 	}
 
 }
+
+
+//NOTES when you have several catch blocks,
+// subclass exceptions should caught before the superclass exception
+// Check out which exception is subclass of other from here: 
+// https://docs.oracle.com/javase/8/docs/api/java/lang/Exception.html
+
