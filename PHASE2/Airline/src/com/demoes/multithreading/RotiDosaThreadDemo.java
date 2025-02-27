bbfe6646e6bc13d59aad2fa5c11affb17f2e0719
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2025-02-27 14:05:54 +0530

    updates

--- PHASE2/Airline/src/com/demoes/multithreading/RotiDosaThreadDemo.java
+++ PHASE2/Airline/src/com/demoes/multithreading/RotiDosaThreadDemo.java
@@ -12,12 +12,15 @@ public class RotiDosaThreadDemo {
 		
 		Thread rotiChef1Thread = new Thread(rotiChef1);	
 		rotiChef1Thread.setName("<ROTI CHEF 1>");
+		rotiChef1Thread.setPriority(10);
 		
 		Thread dosaChef1Thread = new Thread(dosaChef1);
-		dosaChef1Thread.setName("<DOSA CHEF>");
+		dosaChef1Thread.setName("<DOSA CHEF 1>");
+		
 		
 		Thread rotiChef2Thread = new Thread(rotiChef2);	
 		rotiChef2Thread.setName("<ROTI CHEF 2>");
+		rotiChef1Thread.setPriority(2);
 		
 		rotiChef1Thread.start();
 		dosaChef1Thread.start();
