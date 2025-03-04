222208f8d9ea0f141539168e18160f446f923534
aw1 <aw1app@outlook.com>
2025-03-04 15:16:15 +0530

    updates

---
+++ PHASE2/FiorwardRedirectIncludeDemo/src/main/java/com/sl/ServletA.java
@@ -0,0 +1,31 @@
+package com.sl;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/A")
+public class ServletA extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		PrintWriter out = response.getWriter();
+		out.append("output 1 from servlet A");
+		
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/B");
+		
+		out.append("output 2 from servlet A");
+		
+		out.close();
+		
+	}
+
+}
