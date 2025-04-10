a447a379ad45df334af965b7ac6a208b3084bf41
aw1 <aw1app@outlook.com>
2025-04-10 15:44:31 +0530

    updates

--- PHASE3/demo-spring-rest.zip_expanded/demo-spring-rest/src/main/java/com/sl/controller/ProductController.java
+++ PHASE3/demo-spring-rest.zip_expanded/demo-spring-rest/src/main/java/com/sl/controller/ProductController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -31,14 +32,16 @@ public class ProductController {
 	
 	// DISPLAY SINGLE PRODUCT
 	@GetMapping("/details/{id}")
-	public Product getProduct(@PathVariable("id") int id){
+	public ResponseEntity<Product> getProduct(@PathVariable("id") int id){
 		Optional<Product> optionalProduct = productRepositry.findById(id);
 
 		Product p= null;
 		if (optionalProduct.isPresent()) {
 			p = optionalProduct.get();
+			return ResponseEntity.ok().body(p);
 		}
-		return p;
+		
+		return ResponseEntity.notFound().build();
 	}
 	
 	// INSERT A NEW PRODUCT
