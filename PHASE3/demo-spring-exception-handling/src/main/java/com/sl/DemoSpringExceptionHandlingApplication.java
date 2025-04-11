af3cb9ae1760dffea3beeef299ed988702100dff
aw1 <aw1app@outlook.com>
2025-04-11 10:53:37 +0530

    updates

---
+++ PHASE3/demo-spring-exception-handling/src/main/java/com/sl/DemoSpringExceptionHandlingApplication.java
@@ -0,0 +1,19 @@
+package com.sl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@ComponentScan(basePackages = {"com.sl.controller"})
+@EnableJpaRepositories(basePackages = {"com.sl.repositry"})
+@EntityScan(basePackages = "com.sl.entity")
+@SpringBootApplication
+public class DemoSpringExceptionHandlingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringExceptionHandlingApplication.class, args);
+	}
+
+}
---
+++ PHASE3/demo-spring-exception-handling/src/main/java/com/sl/controller/ProductController.java
@@ -0,0 +1,57 @@
+package com.sl.controller;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.sl.entity.Product;
+import com.sl.repositry.ProductRepositry;
+
+@Controller
+public class ProductController {
+	
+	@Autowired
+	ProductRepositry productRepositry;
+	
+	@GetMapping("/list-products")
+	public String listOfProducts(Model model) {
+
+		List<Product> listOfProducts = productRepositry.findAll();
+
+		model.addAttribute("listOfProducts", listOfProducts);
+		
+		// manually let's create a exception situation and observe how
+		// end user will see this in thier browser
+		if(true)
+		throw new RuntimeException("Some error happened");
+
+		return "list-products"; // resolve list-products.jsp in WEB-INF/views
+	}
+	
+	//Exception handling for above RuntimeException
+	@ExceptionHandler(RuntimeException.class)
+	public ResponseEntity<Object> handleRuntimeException( RuntimeException ex){
+//		
+//		Map<String, Object> error = new HashMap<>();
+//        error.put("timestamp", LocalDateTime.now());
+//        error.put("message", ex.getMessage());
+//        error.put("status", HttpStatus.NOT_FOUND.value());
+//        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+		
+		return new ResponseEntity<>(
+				"<b>OOPs!!</b> something went wrong. Contact our CC <i>900123456</i>",
+				HttpStatus.NOT_FOUND);
+		
+	}
+
+
+}
